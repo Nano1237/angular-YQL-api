61009ed10dff0eec10d6b93cc29568110fe512a2
Tim <tim.ruecker@ymail.com>
2015-01-22 12:17:14 +0100

    updated the docu

--- README.md
+++ README.md
@@ -4,4 +4,29 @@ Have you ever had a problem with (for example) the same origin policy when using
 
 Lets say you want to use a external RSS feed inside of your angularjs app. How can you receive the XML without a backend?
 
-The solution is the YQL (Yahoo Query Language) api. How the api exacly works can you read [here](https://developer.yahoo.com/yql).
+The solution is the YQL (Yahoo Query Language) api. 
+###Usage
+
+- Set the `angular-YQL-api` as dependency
+```
+angular.module('myApp', [
+    'angular-YQL-api'
+]);
+```
+- Include the `YQL` factory
+```
+myApp.controller('ExampleCtrl',[
+    'YQL',
+    function(YQL) {
+       //your code goes here
+    }
+]);
+```
+- Get any Website you like
+```
+YQL.html('https://www.google.com/').then(function(a){
+    console.log('google: ',a);
+});
+```
+
+How the api exacly works can you read [here](https://developer.yahoo.com/yql).
--- angular-YQL-api.js
+++ angular-YQL-api.js
@@ -1,51 +1,60 @@
-angular.module('angular-YQL-api', [])
+/**
+ *
+ * angular-YQL-api v0.1
+ * (c) 2015 Tim Rücker
+ * License: Apache License
+ */
+(function(angular) {
+    'use strict';
+    angular.module('angular-YQL-api', [])
 
-        /**
-         * @description The Api Url of the YQL-api
-         */
-        .constant('angular-YQL-api.apiUrl', 'https://query.yahooapis.com/v1/public/yql?q=')
+            /**
+             * @description The Api Url of the YQL-api
+             */
+            .constant('angular-YQL-api.apiUrl', 'https://query.yahooapis.com/v1/public/yql?q=')
 
-        /**
-         * 
-         * @description The api Service for jsonp calls to the YQL-api
-         * @param {type} apiUrl The Target destination of the jsonp call
-         * @param {type} $http The Ajax Service
-         * @param {type} $q The Async reponse service
-         * @returns {unresolved}
-         */
-        .factory('angular-YQL-api.api', [
-            'angular-YQL-api.apiUrl',
-            '$http',
-            '$q',
-            function(apiUrl, $http, $q) {
-                return function(query, options) {
-                    options = typeof options !== 'object' ? {} : options;
-                    options.returnFormat = options.returnFormat || 'json';
-                    var deferred = $q.defer();
-                    var proxy_url = apiUrl + encodeURIComponent(query) + '&format=' + options.returnFormat + '&diagnostics=false&callback=JSON_CALLBACK';
-                    $http.jsonp(proxy_url).success(deferred.resolve);
-                    return deferred.promise;
-                };
-            }
-        ])
+            /**
+             * 
+             * @description The api Service for jsonp calls to the YQL-api
+             * @param {String} apiUrl The Target destination of the jsonp call
+             * @param {Object} $http The Ajax Service
+             * @param {Object} $q The Async reponse service
+             * @returns {Object}
+             */
+            .factory('angular-YQL-api.api', [
+                'angular-YQL-api.apiUrl',
+                '$http',
+                '$q',
+                function(apiUrl, $http, $q) {
+                    return function(query, options) {
+                        options = typeof options !== 'object' ? {} : options;
+                        options.returnFormat = options.returnFormat || 'json';
+                        var deferred = $q.defer();
+                        var proxy_url = apiUrl + encodeURIComponent(query) + '&format=' + options.returnFormat + '&diagnostics=false&callback=JSON_CALLBACK';
+                        $http.jsonp(proxy_url).success(deferred.resolve);
+                        return deferred.promise;
+                    };
+                }
+            ])
 
-        /**
-         * 
-         * @description Provides a list of useful methods for the api calls
-         * @param {type} api The Api Service for the Calls
-         * @returns {_L37.Anonym$3}
-         */
-        .factory('YQL', [
-            'angular-YQL-api.api',
-            function(api) {
-                return {
-                    api: api,
-                    html: function(url, format) {
-                        var yql = 'select * from html where url="' + url + '"';
-                        return this.api(yql, {
-                            returnFormat: format || 'text'
-                        });
-                    }
-                };
-            }
-        ]);
+            /**
+             * 
+             * @description Provides a list of useful methods for the api calls
+             * @param {Object} api The Api Service for the Calls
+             * @returns {_L37.Anonym$3}
+             */
+            .factory('YQL', [
+                'angular-YQL-api.api',
+                function(api) {
+                    return {
+                        api: api,
+                        html: function(url, format) {
+                            var yql = 'select * from html where url="' + url + '"';
+                            return this.api(yql, {
+                                returnFormat: format || 'text'
+                            });
+                        }
+                    };
+                }
+            ]);
+})(angular);
